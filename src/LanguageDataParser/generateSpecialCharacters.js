384510973c491209ac839142047b8e2d3e9c6e0d
Adam Jagosz <adamjagosz@gmail.com>
2019-09-22 19:35:16 +0200

    modify special character list generator

--- src/LanguageDataParser/generateSpecialCharacters.js
+++ src/LanguageDataParser/generateSpecialCharacters.js
@@ -1,16 +1,29 @@
 import genCharArray from "./generateCharArray";
 
 export default function (entry, originalEntry) {
-  let pangram = (entry.alphabet && [entry.alphabet]
-    || entry.pangrams
-    || [ ...entry.letteringsm, ...entry.paragraphs, ...entry.sentences]
-  ).join('');
+  let letters = [];
+  if (originalEntry.alphabet) {
+    letters = originalEntry.alphabet.split(' ');
+  }
+  else {
+    let pangram = (entry.pangrams
+      || [ ...entry.letteringsm, ...entry.paragraphs, ...entry.sentences]
+    ).join('');
+    let str = pangram.toLowerCase() + pangram.toUpperCase()
+    // won't work for combining diacritics - TODO
+    const set = new Set(str.split(''));
+    letters = Array.from(set)
+  }
+  
   let specialCharacters = '';
   if (entry.script == 'Latn') {
-    const abc = genCharArray('a', 'z');
-    const letters = pangram.replace(new RegExp(`[${abc.join('')} ,.;:!?"'„“”‘’()–—-]`, 'gi'), '');
-    const set = new Set(letters.toLowerCase() + letters.toUpperCase());
-    specialCharacters = Array.from(set)
+    const abc = genCharArray('a', 'z').join('');
+    const punct = ' ,.;:!?"\'„“”‘’()\\[\\]{}–—-';
+    const regex = new RegExp(`^[${abc + punct}]$`, 'i');
+    specialCharacters = letters
+      .filter(c => c)
+      .filter(c => c !== ' ')
+      .filter(c => !regex.test(c))
       .sort((a, b) => a.localeCompare(b, entry.htmlTag + '-Latn', { caseFirst: 'upper' }))
       .join(' ');
   }
